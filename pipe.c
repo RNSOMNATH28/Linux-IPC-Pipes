c0ca55f916c12b1013898082567068f9532535b4
somnath <somnath@localhost.localdomain>
2025-11-17 00:31:10 -0500

    success

--- pipe.c
+++ pipe.c
@@ -1,31 +1,3 @@
-# Linux-IPC--Pipes
-Linux-IPC-Pipes
-
-
-# Ex03-Linux IPC - Pipes
-
-# AIM:
-To write a C program that illustrate communication between two process using unnamed and named pipes
-
-# DESIGN STEPS:
-
-### Step 1:
-
-Navigate to any Linux environment installed on the system or installed inside a virtual environment like virtual box/vmware or online linux JSLinux (https://bellard.org/jslinux/vm.html?url=alpine-x86.cfg&mem=192) or docker.
-
-### Step 2:
-
-Write the C Program using Linux Process API - pipe(), fifo()
-
-### Step 3:
-
-Testing the C Program for the desired output. 
-
-# PROGRAM:
-
-## C Program that illustrate communication between two process using unnamed pipes using Linux API system calls
-
-```
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -134,12 +106,3 @@ int main() {
 }
 
 
-```
-
-## OUTPUT
-
-![alt text](<Screenshot at 2025-11-17 00-23-44.png>)
-
-![alt text](<Screenshot at 2025-11-17 00-26-08.png>)
-# RESULT:
-The program is executed successfully.
